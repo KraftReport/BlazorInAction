613804c39a72ab7af9c828e1fa41849864cfaea8
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2025-04-11 15:35:02 +0630

    trail add feature client part

---
+++ blazortrailsshared/Features/ManageTrails/TrailDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blazortrailsshared.Features.ManageTrails
+{
+    public class TrailDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public int TimeInMinutes { get; set; }
+        public int Length { get; set; }
+        public List<RouteInstruction> RouteInstructions { get; set; } = new List<RouteInstruction>();
+
+        public class RouteInstruction
+        {
+            public int State { get; set; }
+            public string Description { get; set; } = string.Empty;
+        }
+    }
+}
