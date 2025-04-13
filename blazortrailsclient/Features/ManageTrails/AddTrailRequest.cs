ef381ee5f4256b5c3b36b9f31a7e21210253a4d0
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2025-04-13 18:24:01 +0630

    implementing mediator pattern

---
+++ blazortrailsclient/Features/ManageTrails/AddTrailRequest.cs
@@ -0,0 +1,21 @@
+﻿using blazortrailsshared.Features.ManageTrails;
+using FluentValidation;
+using MediatR;
+
+namespace blazortrailsclient.Features.ManageTrails
+{
+    public record AddTrailRequest(TrailDto TrailDto) : IRequest<AddTrailRequest.Response> 
+    {
+        public const string RouteTemplate = "/api/trails";
+
+        public record Response(int TrailId);
+    }
+
+    public class AddTrailRequestValidator : AbstractValidator<AddTrailRequest>
+    {
+        public AddTrailRequestValidator()
+        {
+            RuleFor(x => x.TrailDto).SetValidator(new TrailValidattor());
+        }
+    }
+}
--- blazortrailsclient/Program.cs
+++ blazortrailsclient/Program.cs
@@ -1,4 +1,5 @@
 using blazortrailsclient;
+using MediatR;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -7,6 +8,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMediatR(typeof(Program).Assembly);
 
 builder.Logging.SetMinimumLevel(LogLevel.Debug);
 
