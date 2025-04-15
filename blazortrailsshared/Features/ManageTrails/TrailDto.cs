364ea48c94c3ce7b92714012d316f2e028a1b454
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2025-04-15 16:28:53 +0630

    creating custom input for time

--- blazortrailsshared/Features/ManageTrails/TrailDto.cs
+++ blazortrailsshared/Features/ManageTrails/TrailDto.cs
@@ -26,6 +26,7 @@ namespace blazortrailsshared.Features.ManageTrails
             RuleFor(x => x.Description).NotEmpty().WithMessage("Please enter a description");
             RuleFor(x => x.Location).NotEmpty().WithMessage("Please enter a location");
             RuleFor(x => x.Length).GreaterThan(0).WithMessage("Please enter a length");
+            RuleFor(x => x.TimeInMinutes).GreaterThan(0).WithMessage("Please enter a time");
             RuleFor(x => x.RouteInstructions).NotEmpty().WithMessage("Please add one route instruction");
             RuleForEach(x => x.RouteInstructions).SetValidator(new RouteInstructionValidator());
         }
