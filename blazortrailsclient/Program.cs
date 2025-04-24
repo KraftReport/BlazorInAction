46f3868451975232bb8b307f571292b28c32cdb7
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2025-04-24 15:21:28 +0630

    error fix

--- blazortrailsclient/Program.cs
+++ blazortrailsclient/Program.cs
@@ -2,6 +2,7 @@ using blazortrailsclient;
 using MediatR;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Options;
 using System.Security.Claims;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -10,15 +11,17 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddMediatR(typeof(Program).Assembly);
-
-builder.Logging.SetMinimumLevel(LogLevel.Debug);
 
 builder.Services.AddOidcAuthentication(option =>
 {
     builder.Configuration.Bind("Auth0", option.ProviderOptions);
-    option.ProviderOptions.ResponseType = "code";
+    option.ProviderOptions.ResponseType ="code"; 
     option.UserOptions.NameClaim = ClaimTypes.GivenName;
+    option.ProviderOptions.AdditionalProviderParameters.Add("audience", "https://blazortrails.com");
 });
+
+builder.Services.AddMediatR(typeof(Program).Assembly);
+
+builder.Logging.SetMinimumLevel(LogLevel.Debug);
 
 await builder.Build().RunAsync();
