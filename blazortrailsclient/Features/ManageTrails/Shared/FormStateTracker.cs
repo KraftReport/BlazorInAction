77bff05d17ac4f082dbf713f28d52dadc0239608
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2025-04-25 18:00:48 +0630

    state management with simple app store

---
+++ blazortrailsclient/Features/ManageTrails/Shared/FormStateTracker.cs
@@ -0,0 +1,35 @@
+﻿using blazortrailsclient.State;
+using blazortrailsshared.Features.ManageTrails.Shared;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace blazortrailsclient.Features.ManageTrails.Shared
+{
+    public class FormStateTracker : ComponentBase
+    {
+        [Inject]
+        public AppState AppState { get;set; }
+        [CascadingParameter]
+        private EditContext CascadingEditContext { get; set; }
+
+        protected override void OnInitialized()
+        {
+            if(CascadingEditContext is null)
+            {
+                throw new InvalidOperationException(
+                    $"{nameof(CascadingEditContext)} is required for parameter type fo {nameof(EditContext)}");
+            }
+            CascadingEditContext.OnFieldChanged += CascadingEditContext_OnFiledChanged;
+        }
+
+        private void CascadingEditContext_OnFiledChanged(object sender,FieldChangedEventArgs e)
+        {
+            var trail = e.FieldIdentifier.Model as TrailDto;
+            Console.WriteLine($"Field changed: {e.FieldIdentifier.FieldName}");
+            if (trail.Id == 0)
+            {
+                AppState.SetUnSavedNewTrailDto(trail);
+            }
+        }
+    }
+}
---
+++ blazortrailsshared/Features/ManageTrails/Shared/Utils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blazortrailsshared.Features.ManageTrails.Shared
+{
+    public class Utils
+    {
+        public const string baseUrl = "https://localhost:7057";
+    }
+}
