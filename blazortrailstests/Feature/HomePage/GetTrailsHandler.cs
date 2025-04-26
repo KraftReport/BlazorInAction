9e06f3b8c47897fd9f8ad4b9c531fa286c574c21
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2025-04-27 03:42:44 +0630

    add unit testing and integration tesing with buint for homepage component

---
+++ blazortrailstests/Feature/HomePage/GetTrailsHandler.cs
@@ -0,0 +1,22 @@
+﻿using AutoFixture;
+using blazortrailsshared.Features.ManageTrails.Shared;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace blazortrailstests.Feature.HomePage
+{
+    public class GetTrailsHandler : IRequestHandler<GetTrailsRequest, GetTrailsRequest.Response>
+    {
+        public async Task<GetTrailsRequest.Response> Handle(GetTrailsRequest request, CancellationToken cancellationToken)
+        {
+            var fixture = new Fixture();
+            var trails = fixture.CreateMany<GetTrailsRequest.Trail>();
+            return  new GetTrailsRequest.Response(trails);
+        }
+    }
+}
